71bdf207b04feb093bd2dff7226d6a8d0c5ab3e6
ilya <011283im@gmail.com>
2022-05-17 11:25:29 +0300

    добавил в интерфейс два метода, перерисовки списка по номеру ячейки и метод получения данных о списке всех заметок

--- app/src/main/java/com/example/edunotekotlin/ui/ViewInterface.kt
+++ app/src/main/java/com/example/edunotekotlin/ui/ViewInterface.kt
@@ -1,7 +1,11 @@
 package com.example.edunotekotlin.ui
 
+import com.example.kotlineasynote.entities.OneNote
+
 interface ViewInterface {
     fun redraw()
+    fun redrawRecyclerInPosition(position:Int)
+    fun writeNoteListToData(list: MutableList<OneNote>)
     fun startLoading()
     fun loaded()
 }
