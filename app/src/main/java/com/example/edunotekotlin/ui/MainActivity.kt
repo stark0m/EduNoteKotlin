bc3345f7ab1a883e633e0f4f94f9d5f793bfef48
ilya <011283im@gmail.com>
2022-05-17 11:50:53 +0300

    добавил в лейауты необходимые компоненты, добавил в мейнактивити отображение фрагмента

--- app/src/main/java/com/example/edunotekotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/edunotekotlin/ui/MainActivity.kt
@@ -5,8 +5,17 @@ import android.os.Bundle
 import com.example.edunotekotlin.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fragment: MainFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        if (savedInstanceState==null) {
+            fragment = MainFragment()
+                    }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container,fragment)
+            .commit()
     }
 }
