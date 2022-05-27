14882000d8b22784ecf7eca6f711867da26ca15b
ilya <011283im@gmail.com>
2022-05-27 12:51:48 +0300

    добавили топменю во фрагмент, и добавили в него дровлерменю

--- app/src/main/java/com/example/edunotekotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/edunotekotlin/ui/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.edunotekotlin.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.edunotekotlin.R
+import com.example.edunotekotlin.entities.MenuDrawable
 import com.example.edunotekotlin.ui.main.MainFragment
+import com.google.android.material.appbar.MaterialToolbar
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(),MenuDrawable {
+    lateinit var drawler: DrawerLayout
     val fragment = MainFragment().newInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawler = findViewById(R.id.drawer_layout)
+
 
 
         if (savedInstanceState==null) {
@@ -19,4 +27,16 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragment_container,fragment)
             .commit()
     }}
+
+    override fun setAppToolbar(toolbar: MaterialToolbar) {
+        var toggle = ActionBarDrawerToggle(
+            this,
+            drawler,
+            toolbar,
+            R.string.open_rawler,
+            R.string.close_drawler
+        )
+        drawler.addDrawerListener(toggle)
+        toggle.syncState()
+    }
 }
