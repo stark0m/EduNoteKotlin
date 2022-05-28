56402f574f0821059af6bfb2007b388c160dd8c1
ilya <011283im@gmail.com>
2022-05-28 12:02:41 +0300

    попробуем презентер переделать в объект

--- app/src/main/java/com/example/edunotekotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/edunotekotlin/ui/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity(),MenuDrawable {
     lateinit var drawler: DrawerLayout
     lateinit var navigationDrawlerView: NavigationView
     val fragment = MainFragment().newInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
