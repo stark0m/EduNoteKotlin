e356da91773799c5fb95b0263ca98ac7bfd044a0
ilya <011283im@gmail.com>
2022-05-31 09:08:46 +0300

    добавил обработчик двойного нажатия кнопки назад

--- app/src/main/java/com/example/edunotekotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/edunotekotlin/ui/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.edunotekotlin.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.edunotekotlin.R
 import com.example.edunotekotlin.entities.MenuDrawable
@@ -11,10 +12,11 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
 
 
-class MainActivity : AppCompatActivity(),MenuDrawable {
+class MainActivity : AppCompatActivity(), MenuDrawable {
     lateinit var drawler: DrawerLayout
     lateinit var navigationDrawlerView: NavigationView
     val fragment = MainFragment().newInstance()
+    private var back_pressed: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,13 +24,14 @@ class MainActivity : AppCompatActivity(),MenuDrawable {
         navigationDrawlerView = findViewById((R.id.navigation_view))
 
 
-        if (savedInstanceState==null) {
+        if (savedInstanceState == null) {
 
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container,fragment)
-            .commit()
-    }}
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
 
     override fun setAppToolbar(toolbar: MaterialToolbar) {
         var toggle = ActionBarDrawerToggle(
@@ -41,7 +44,7 @@ class MainActivity : AppCompatActivity(),MenuDrawable {
         drawler.addDrawerListener(toggle)
         toggle.syncState()
         navigationDrawlerView.setNavigationItemSelectedListener {
-            when (it.itemId){
+            when (it.itemId) {
                 R.id.action_add -> {
                     fragment.presenter.addNote()
                     drawler.close()
@@ -52,4 +55,15 @@ class MainActivity : AppCompatActivity(),MenuDrawable {
         }
 
     }
+
+    override fun onBackPressed() {
+
+        if (back_pressed + 2000 > System.currentTimeMillis()) {
+            // TODO:  super.onBackPressed()
+        } else {
+            Toast.makeText(baseContext, "Press once again to exit!", Toast.LENGTH_SHORT).show()
+        }
+        back_pressed = System.currentTimeMillis()
+    }
+
 }
